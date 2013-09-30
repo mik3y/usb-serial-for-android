d27202103418df588d80fa32e8e22b9b80b94c1b
Arthur Benemann <arthur.benemann@gmail.com>
2013-09-30 15:03:23 -0300

    Adding support for the new FT231X IC from FTDI
    To use it as a serial-USB bridge it's the same thing as a FT232 IC

--- UsbSerialLibrary/src/com/hoho/android/usbserial/driver/UsbId.java
+++ UsbSerialLibrary/src/com/hoho/android/usbserial/driver/UsbId.java
@@ -31,6 +31,7 @@ public final class UsbId {
 
     public static final int VENDOR_FTDI = 0x0403;
     public static final int FTDI_FT232R = 0x6001;
+    public static final int FTDI_FT231X = 0x6015;
 
     public static final int VENDOR_ATMEL = 0x03EB;
     public static final int ATMEL_LUFA_CDC_DEMO_APP = 0x2044;
