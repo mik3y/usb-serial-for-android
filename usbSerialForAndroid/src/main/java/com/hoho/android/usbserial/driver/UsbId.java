74b1c03441b68eec30f85fdf59e29e475924e47e
Eric <eric.s@ybrain.com>
2015-01-12 16:44:55 +0900

    Added VID/PID to support STM32_F373XX based devices.

--- usbSerialForAndroid/src/main/java/com/hoho/android/usbserial/driver/UsbId.java
+++ usbSerialForAndroid/src/main/java/com/hoho/android/usbserial/driver/UsbId.java
@@ -64,6 +64,9 @@ public final class UsbId {
     public static final int VENDOR_PROLIFIC = 0x067b;
     public static final int PROLIFIC_PL2303 = 0x2303;
 
+    public static final int VENDOR_STMICROELECTRONICS = 0x0483; 
+    public static final int STM32_F373XX = 0x5740; 
+    
     private UsbId() {
         throw new IllegalAccessError("Non-instantiable class.");
     }
