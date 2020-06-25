aee7fc1b9d9cf4cd3365ae919d8c34b9bbd8e8cc
rusefi <rCbUkxuY64GWzD2>
2020-06-24 20:38:52 -0400

    ST CDC See https://www.the-sz.com/products/usbid/index.php?v=0483&p=&n=

--- usbSerialForAndroid/src/main/java/com/hoho/android/usbserial/driver/CdcAcmSerialDriver.java
+++ usbSerialForAndroid/src/main/java/com/hoho/android/usbserial/driver/CdcAcmSerialDriver.java
@@ -333,6 +333,10 @@ public class CdcAcmSerialDriver implements UsbSerialDriver {
                 new int[] {
                     UsbId.ARM_MBED,
                 });
+        supportedDevices.put(UsbId.VENDOR_ST,
+                new int[] {
+                        UsbId.ST_CDC,
+                });
         return supportedDevices;
     }
 
