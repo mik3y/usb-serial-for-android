2d13b90f59eec45adadaeed1aa593fe9a0d7540b
kai-morich <mail@kai-morich.de>
2020-07-27 17:35:14 +0200

    move from LGPL to MIT license (#244)
    moving away from LGPL possible, as the FTDI driver is rewritten and not based any more on LGPL code from libftdi.

--- usbSerialForAndroid/src/main/java/com/hoho/android/usbserial/driver/ProlificSerialDriver.java
+++ usbSerialForAndroid/src/main/java/com/hoho/android/usbserial/driver/ProlificSerialDriver.java
@@ -1,28 +1,10 @@
-/* This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+/*
+ * Ported to usb-serial-for-android by Felix Hädicke <felixhaedicke@web.de>
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
- * USA.
+ * Based on the pyprolific driver written by Emmanuel Blot <emmanuel.blot@free.fr>
+ * See https://github.com/eblot/pyftdi
  *
  * Project home page: https://github.com/mik3y/usb-serial-for-android
- */
-
-/*
- * Ported to usb-serial-for-android
- * by Felix Hädicke <felixhaedicke@web.de>
- *
- * Based on the pyprolific driver written
- * by Emmanuel Blot <emmanuel.blot@free.fr>
- * See https://github.com/eblot/pyftdi
  */
 
 package com.hoho.android.usbserial.driver;
